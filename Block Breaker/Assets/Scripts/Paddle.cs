66082b13afab553319c0fff57888bcbb03ca9b4a
Luigi Lantin <mushymanee@gmail.com>
2021-02-01 16:19:04 -0800

    updated paddle behavior

--- Block Breaker/Assets/Scripts/Paddle.cs
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    // configuration parameters
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
 
     [SerializeField] float screenWidthInUnits = 16f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +20,8 @@ public class Paddle : MonoBehaviour
     void Update()
     {
         float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
-        Vector2 paddlePos = new Vector2(mousePosInUnits, transform.position.y);
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
         transform.position = paddlePos;
-
     }
 }
