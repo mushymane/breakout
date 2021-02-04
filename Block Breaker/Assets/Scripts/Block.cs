d79cb8fc53983e2d6e5a36df61c9fac331c92241
Luigi Lantin <mushymanee@gmail.com>
2021-02-04 04:16:31 -0800

    fixed bug where ball would get stuck moving across an axis

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class Block : MonoBehaviour
 {
-
     // configuration parameters
     [SerializeField] AudioClip breakSound;
     [SerializeField] GameObject blockSparklesVFX;
